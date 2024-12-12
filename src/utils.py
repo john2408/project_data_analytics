be7a726c6b07245e1e8c2354c5f4821e96e8ed9d
John Torres <john.torres.consulting@gmail.com>
2024-12-12 20:54:17 +0100

    conclusions and analysis feature importance

--- src/utils.py
+++ src/utils.py
@@ -13,25 +13,24 @@ def read_config(yaml_file_path: str) -> Dict[str, Any]:
     Returns:
         Dict[str, Any]: dict object
     """
-    config = {}
     try:
         with open(yaml_file_path, "r") as file:
-            config = yaml.safe_load(file)
+            return yaml.safe_load(file)
     except FileNotFoundError:
         print(f"The file {yaml_file_path} does not exist.")
     except yaml.YAMLError as exc:
         print(f"Error parsing YAML file: {exc}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+    return {}
 
-    return config
 
-
-def smape(y_true: np.array, y_pred: np.array) -> float:
+def smape(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """Calculate the Symmetric Mean Absolute Error
+
     Args:
-        y_true (np.array): true values
-        y_pred (np.array): forecast values
+        y_true (np.ndarray): true values
+        y_pred (np.ndarray): forecast values
 
     Returns:
         float: SMAPE
@@ -46,11 +45,10 @@ def store_pickle(obj: Any, path: str) -> None:
 
     Args:
         obj (Any): Input Object
+        path (str): Path to store the pickle file
     """
-
     with open(path, "wb") as file:
         pickle.dump(obj, file)
-
     print(f"Object has been stored at {path}.")
 
 
@@ -63,10 +61,7 @@ def read_pickle(path: str) -> Any:
     Returns:
         Any: object
     """
-
     with open(path, "rb") as file:
         obj = pickle.load(file)
-
     print(f"Loaded object from {path}.")
-
     return obj
